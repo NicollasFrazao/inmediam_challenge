afd735623aeb037960818ccc348bbfa1d2a7dba4
Nícollas Frazão <nicollasfrazao@hotmail.com>
2025-07-16 01:25:32 -0300

    feat: add payments method in contract model

--- api/app/Models/Contract.php
+++ api/app/Models/Contract.php
@@ -35,4 +35,9 @@ class Contract extends Model
     {
         return $this->belongsTo(Plan::class);
     }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
