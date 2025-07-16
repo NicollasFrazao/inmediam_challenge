c45a3ea0eeeea0c0e687f58665ce63ffe114eb26
Nícollas Frazão <nicollasfrazao@hotmail.com>
2025-07-16 01:24:47 -0300

    feat: create payment model

---
+++ api/app/Models/Payment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $table = 'payments';
+
+    protected $fillable = [
+        'user_id',
+        'contract_id',
+        'status',
+        'value',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function contract()
+    {
+        return $this->belongsTo(Contract::class);
+    }
+}
