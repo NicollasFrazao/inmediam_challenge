512b7717cb352e63121909d6964fb3fd58417d35
Nícollas Frazão <nicollasfrazao@hotmail.com>
2025-07-16 23:30:47 -0300

    feat: discount value to hire other plan

--- api/app/Http/Controllers/PlanController.php
+++ api/app/Http/Controllers/PlanController.php
@@ -20,20 +20,23 @@ class PlanController extends Controller
     public function hire(Plan $plan)
     {
         $user = User::find(1);
+        $value = $plan->price;
 
         \DB::beginTransaction();
         
-        if ($current_contract = $user->current_contract) {}
-        else
+        if ($current_contract = $user->current_contract) 
         {
-            $price = $plan->price;
-            $discount = 0;
+            $days_of_use = $current_contract->started_at ? $current_contract->started_at->diffInDays(\Carbon\Carbon::now()) : 0;
+            $days_of_contract = $current_contract->started_at->diffInDays($current_contract->ended_at);
+            
+            $discount = $current_contract->value - (($days_of_contract > 0) ? ($current_contract->value*$days_of_use)/$days_of_contract : 0);
         }
+        else $discount = 0;
 
         $contract = $user->contracts()->create([
             'plan_id' => $plan->id,
             'is_active' => false,
-            'value' => $price,
+            'value' => $value,
             'discount' => $discount,
         ]);
 
@@ -41,9 +44,9 @@ class PlanController extends Controller
             'user_id' => $user->id,
             'transaction_id' => \Str::uuid(),
             'status' => 'pending',
-            'value' => $price,
+            'value' => $value - $discount,
         ]);
-        
+
         \DB::commit();
 
         return response()->json([
